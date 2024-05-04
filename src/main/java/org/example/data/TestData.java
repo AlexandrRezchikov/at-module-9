bbbc34b71b3865add06546c4076b6babb9edaa50
Aleksandr_Rezchikov <rez4ikov@gmail.com>
2024-05-04 23:37:15 +0300

    version1

--- src/main/java/org/example/data/TestData.java
+++ src/main/java/org/example/data/TestData.java
@@ -3,9 +3,4 @@ package org.example.data;
 public class TestData {
 
     public static final String URI = "https://reqres.in/api";
-
-    public static final String REQUEST_BODY_UPDATE_USER = "{" +
-            "\"name\": \"morpheus\"," +
-            "\"job\": \"zion resident\"" +
-            "}";
 }
